88299ccd8cdf4f4dd9bf0275e6880dba504276cc
Jeon Hyejin <hyejda00@naver.com>
2024-09-26 00:45:44 +0900

    Header, Main, Footer Component 나누기

---
+++ section05/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Footer from './components/Footer.jsx';
+import Header from './components/Header.jsx';
+import Main from './components/Main.jsx';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <h1>하이루! 리액트</h1>
+      <Footer />
+    </>
+  )
+}
+
+export default App
