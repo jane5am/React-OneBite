6152270fbe8e7c32d89bb22fd32d1f14a0e5ce13
Jeon Hyejin <hyejda00@naver.com>
2024-09-26 02:09:50 +0900

    조건에 따라 UI렌더링하기, 스타일 넣기

--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -1,9 +1,26 @@
+import "./Main.css";
+
 const Main = () => {
-  return (
-    <>
-      <h1>Main</h1>
-    </>
-  );
+  const user = {
+    name : "APPLE",
+    isLogin : true,
+  };
+
+  if( user.isLogin ) {
+    // return <div style= {{
+    //   backgroundColor: "red",
+    //   borderBottom: "5px solid blue",
+    // }}> 로그아웃</div>
+    return <div className="logout">로그아웃</div>
+  }else {
+    return <div>로그인</div>
+  }
+  
+  // return (
+  //   <>
+  //     {user.isLogin ? <div>로그아웃</div> : <div>로그인</div>}
+  //   </>
+  // );
 } 
 
 export default Main;
