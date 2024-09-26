990c993f813ee5b4271cae9944634f29574dfbdc
Jeon Hyejin <hyejda00@naver.com>
2024-09-26 15:52:37 +0900

    컴포넌트 쪼개서 전구만들기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,37 +1,13 @@
 import './App.css'
-import { useState } from 'react';
+import Bulb from './components/Bulb'
+import Counter from './components/Count'
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState("OFF");
-
-  const button = () => {
-    setCount(count+1);
-  }
-
-  const lightButton = () => {
-    setLight( light === "OFF" ? "ON"  : "OFF");
-  }
-
 
   return (
     <>
-      <div>
-        <h1>{light}</h1>
-        <button onClick={lightButton}>
-          { light === "OFF" ? "켜기" : "끄기" }
-        </button>
-        
-      </div>
-      <div>
-      <h1>{count}</h1>
-        <button onClick={button}>+</button>
-        {/* <button
-          onClick={ () => { setCount(count + 1)} }
-          > */}
-          {/* +
-        </button> */}
-      </div>
+      <Bulb />
+      <Counter />
     </>
   )
 }
