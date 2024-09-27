ecfae4f1b54ba44b527fe2d61bcf51b65cb0faef
Jeon Hyejin <hyejda00@naver.com>
2024-09-27 13:08:12 +0900

    카운터 앱 만들기 프로젝트

---
+++ section06/src/components/Controller.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const Controller = ({onClickButton}) => {
+
+  return (
+    <>
+      <button 
+        onClick={ () => {
+          onClickButton(-1);
+        }
+      }>-1</button>
+      <button 
+        onClick={ () => {
+          onClickButton(-10);
+        }
+      }>-10</button>
+      <button 
+        onClick={ () => {
+          onClickButton(-100);
+        }
+      }>-100</button>
+      <button 
+        onClick={ () => {
+          onClickButton(+100);
+        }
+      }>+100</button>
+      <button 
+        onClick={ () => {
+          onClickButton(+10);
+        }
+      }>+10</button>
+           <button 
+        onClick={ () => {
+          onClickButton(+1);
+        }
+      }>+1</button>
+    </>
+  )
+}
+export default Controller;
